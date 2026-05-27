d1e484fba4838f8f65b017c581b188eff3b0959d
Alice Kits <alisha727cute@gmail.com>
2026-05-27 08:54:04 +0200

    fix(dto): add JvmInline to PlayerId

--- dto/src/commonMain/kotlin/models/PlayerId.kt
+++ dto/src/commonMain/kotlin/models/PlayerId.kt
@@ -1,6 +1,8 @@
 package me.nekoalice.mafia.api.dto.models
 
 import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmInline
 
 @Serializable
+@JvmInline
 public value class PlayerId(public val value: String)
