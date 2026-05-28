f3338849279efcc7afc978b4d4a27ece2b873c25
Alice Kits <alisha727cute@gmail.com>
2026-05-28 12:43:54 +0200

    fix(dto): make HelloResponse serializable

--- dto/src/commonMain/kotlin/models/HelloResponse.kt
+++ dto/src/commonMain/kotlin/models/HelloResponse.kt
@@ -1,5 +1,8 @@
 package me.nekoalice.mafia.api.dto.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 public data class HelloResponse(
     val message: String = "Hello, World!",
 )
