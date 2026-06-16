faf72782bbe596db5ac9438f46e390a0cfd23131
Alice Kits <alisha727cute@gmail.com>
2026-06-16 16:41:24 +0500

    refactor(migrations): track applied migration versions, add logging

--- migrations/src/main/kotlin/Main.kt
+++ migrations/src/main/kotlin/Main.kt
@@ -1,16 +1,68 @@
 package me.nekoalice.mafia.api.migrations
 
-import me.nekoalice.mafia.api.migrations.migrations.V01Init
-import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
+import kotlinx.coroutines.flow.single
+import me.nekoalice.mafia.api.migrations.migrations.*
+import org.jetbrains.exposed.v1.core.*
+import org.jetbrains.exposed.v1.r2dbc.*
 import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 // must be ordered by version!
 val migrations = listOf(
     V01Init,
 )
 
+val logger: Logger = LoggerFactory.getLogger("Migrations")
+
 fun env(key: String): String =
     System.getenv(key) ?: throw IllegalArgumentException("Environment variable $key is not set")
+
+object MigrationsTable : Table("migrations") {
+    val version: Column<UInt> = uinteger("version")
+
+    suspend fun versionUp(): UInt = updateReturning {
+        it[version] = version + 1u
+    }.single()[version]
+
+    suspend fun versionDown(): UInt = updateReturning {
+        it[version] = version - 1u
+    }.single()[version]
+
+    suspend fun getVersion(): UInt =
+        MigrationsTable.select(version).single()[version]
+
+    suspend fun createAndInit(): UInt {
+        SchemaUtils.create(MigrationsTable)
+        return try {
+            getVersion()
+        } catch (_: NoSuchElementException) {
+            insertReturning {
+                it[version] = 0u
+            }.single()[version]
+        }
+    }
+}
+
+suspend fun <T> suspendTransactionInContext(
+    block: suspend context(R2dbcTransaction) () -> T,
+): T = suspendTransaction {
+    context(this) {
+        block()
+    }
+}
+
+suspend fun runMigrations(
+    requiredMigrations: List<Migration>,
+    action: suspend context(R2dbcTransaction) (Migration) -> Unit,
+) {
+    logger.info("Found ${requiredMigrations.size} pending migrations")
+    for (migration in requiredMigrations) {
+        suspendTransactionInContext {
+            action(migration)
+        }
+    }
+}
 
 suspend fun main(args: Array<String>) {
     R2dbcDatabase.connect(
@@ -20,23 +72,31 @@ suspend fun main(args: Array<String>) {
         password = env("DATABASE_PASSWORD"),
     )
 
+    val currentVersion: UInt = suspendTransaction {
+        MigrationsTable.createAndInit()
+    }
+
     val action = if (args.isEmpty()) "latest" else args[0]
     // TODO: add up and down by X
     when (action) {
-        "latest" -> migrations.forEach {
-            suspendTransaction {
-                context(this) {
-                    it.up()
-                }
-            }
+        "latest" -> runMigrations(
+            migrations
+                .filter { it.version > currentVersion }
+                .sortedBy { it.version },
+        ) {
+            it.up()
+            MigrationsTable.versionUp()
+            logger.info("Migration ${it::class.simpleName} applied")
         }
 
-        "first" -> migrations.asReversed().forEach {
-            suspendTransaction {
-                context(this) {
-                    it.down()
-                }
-            }
+        "clear" -> runMigrations(
+            migrations
+                .filter { it.version <= currentVersion }
+                .sortedByDescending { it.version },
+        ) {
+            it.down()
+            MigrationsTable.versionDown()
+            logger.info("Migration ${it::class.simpleName} reverted")
         }
 
         else -> throw IllegalArgumentException("Invalid action: $action")
